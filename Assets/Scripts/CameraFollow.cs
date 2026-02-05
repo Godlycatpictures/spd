1a0c7d9386bfcb0b716a0a7e05c2cefb31e8c308
Godlycatpictures <leoon06@outlook.com>
2026-02-05 17:02:59 +0100

    camera

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -7,8 +7,9 @@ public class CameraFollow : MonoBehaviour
     [SerializeField] private Transform playerPos; // hitta spelare
     private GameObject playerObject;
     private PlayerController playerController;
-    [SerializeField] private Vector3 offset = new Vector3(0, 0, -10);
+    [SerializeField] private Vector3 offset = new Vector3(2, 0, -10);
     [SerializeField] private float smoothing = 1f;
+    private float lastDirection = 1;
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -21,13 +22,22 @@ public class CameraFollow : MonoBehaviour
     // Update is called once per frame
     void LateUpdate() // late update kommer i slutet av en frame
     {
-        
+        float direction = playerController.GetDirection();
+                
+        if (direction == 0)
+            direction = lastDirection;
+        else
+            lastDirection = direction;
 
-        if (playerController.GetDirection() < 0)
-            offset.x = playerPos.position.x; 
+        Vector3 directionOffset = offset;
 
-        Vector3 newPosition = Vector3.Slerp(transform.position, playerPos.position + offset, smoothing * Time.deltaTime);
+        if (direction < 0)
+            directionOffset.x = -Mathf.Abs(offset.x);
+        else if (direction > 0)
+            directionOffset.x = Mathf.Abs(offset.x);
 
-        transform.position = newPosition;
+
+        Vector3 newPosition = playerPos.position + directionOffset;
+        transform.position = Vector3.Lerp(transform.position, newPosition, smoothing * Time.deltaTime);
     }
 }
