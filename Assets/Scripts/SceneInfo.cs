6a69d751a33c18e9081632ef76d2365f0d4d7f76
Godlycatpictures <leoon06@outlook.com>
2026-02-12 11:08:13 +0100

    flyttade runt filer + sceneinfo

---
+++ Assets/Scripts/SceneInfo.cs
@@ -0,0 +1,48 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "SceneInfo", menuName = "Persistence")]
+public class SceneInfo : ScriptableObject
+
+{
+    [SerializeField] private int collectedGnomes;
+    [SerializeField] private int killedWizards;
+
+    // Metod för att tillämpa uppgraderingar på karaktären
+    public void ResetSceneInfo()
+    {
+        collectedGnomes = 0; 
+        killedWizards = 0;
+
+
+
+    }
+
+    public int GetKilledWizards()
+    {
+        return killedWizards;
+    }
+    public int GetGnomes()
+    {
+        return collectedGnomes;
+    }
+    public void AddGnomes()
+    {
+        collectedGnomes++;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
