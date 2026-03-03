34cb9ff3c73dcea855ba95647f7c1c3f2c8d89e0
Godlycatpictures <leoon06@outlook.com>
2026-03-03 12:36:01 +0100

    typ klar helt ärligt

--- Assets/Scripts/PlayerScripts/SwordSwing.cs
+++ Assets/Scripts/PlayerScripts/SwordSwing.cs
@@ -15,6 +15,13 @@ public class SwordSwing : MonoBehaviour
 
     [SerializeField] private SceneInfo sceneInfo;
 
+    [SerializeField] private Transform playerPos;
+    [SerializeField] private Vector3 offset = new Vector3(2, 0, 5);
+    private float lastDirection = 1;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    
 
     private void Awake()
     {
@@ -26,9 +33,22 @@ public class SwordSwing : MonoBehaviour
 
     private void Update()
     {
+        float direction = playerController.GetDirection();
+
+        if (direction == 0)
+            direction = lastDirection;
+        else
+            lastDirection = direction;
+
+        Vector3 directionOffset = new Vector3(offset.x * direction, offset.y, offset.z); 
+
         if (attackAction.WasPressedThisFrame() &&  sceneInfo.HasSword() == true) 
         {
-            Instantiate(PlayerSword);
+            Quaternion rotation = direction > 0 ? Quaternion.Euler(0, 0, -90) : Quaternion.Euler(0, 0, 90);
+            GameObject swordInstance = Instantiate(PlayerSword, playerPos.position + new Vector3(offset.x * direction, offset.y, offset.z), rotation);
+            SwordLifetime swordLifetime = swordInstance.GetComponent<SwordLifetime>();
+            if (swordLifetime != null)
+                swordLifetime.Setup(playerPos, playerController, offset);
         }
     }
 }
--- Assets/Scripts/SwordLifetime.cs
+++ Assets/Scripts/SwordLifetime.cs
@@ -3,11 +3,17 @@ using UnityEngine;
 public class SwordLifetime : MonoBehaviour
 {
     [SerializeField] private float lifetime = 1.5f;
+    private Transform playerTransform;
+    private PlayerController playerController;
+    private Vector3 offset;
+    private float lastDirection = 1;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public void Setup(Transform player, PlayerController controller, Vector3 swordOffset)
     {
-        
+        playerTransform = player;
+        playerController = controller;
+        offset = swordOffset;
     }
 
     private void Update()
@@ -17,7 +23,20 @@ public class SwordLifetime : MonoBehaviour
         if (lifetime <= 0)
         {
             Destroy(gameObject);
+            return;
         }
+        
+        float direction = playerController.GetDirection();
+        
+
+        if (direction == 0)
+            direction = lastDirection;
+        else
+            lastDirection = direction;
+
+        transform.position = playerTransform.position + new Vector3(offset.x * direction, offset.y, offset.z);
+        
+        transform.rotation = direction > 0 ? Quaternion.Euler(0, 0, -90) : Quaternion.Euler(0, 0, 90);
     }
 
 
